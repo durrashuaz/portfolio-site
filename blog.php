c0fd2aa30c05534405d193ad29e95ee11914cd52
Durra Shuazlan <d.shuazlan@se19.qmul.ac.uk>
2022-10-11 11:54:44 +0100

    Add blog listing page

---
+++ blog.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<?php
+require "components/_head-header.php";
+?>
+
+<div class='bg bg--light bg--lines1'>
+    <div class='section'>
+        <div class='container'>
+            No blog entries at the moment. Check out <a class="a" href="/portfolio.php">Portfolio entries.</a>
+        </div>
+    </div>
+</div>
+
+<?php require "components/_footer.html" ?>
+
+</body>
+</html>
