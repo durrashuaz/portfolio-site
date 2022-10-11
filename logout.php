d95368d4319b2bb7973f784688c917fd9bc02c54
Durra Shuazlan <d.shuazlan@se19.qmul.ac.uk>
2022-10-11 11:29:41 +0100

    Delete all files

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start(); //to gain access to global session variables
-session_destroy();  //destroy the global variables and end session
-
-header('Location: /webroot/finalWebMini/index.php');
-?>
--- yasmine.php
+++
@@ -1,8 +0,0 @@
-
-            <!DOCTYPE html>
-            <html>
-            <?php
-            require "components/_head-header.php";
-            include "connect.php";
-            session_start();
-            
