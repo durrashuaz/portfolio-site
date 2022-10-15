06bce42269ed45bdd6049992ec3962c17f3902af
Durra Shuazlan <d.shuazlan@se19.qmul.ac.uk>
2022-10-15 14:04:32 +0100

    Add test

--- _login.php
+++ _login.php
@@ -18,31 +18,26 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' ){
 	if( $sql-> num_rows > 0 ){
 
 		while( $rows = mysqli_fetch_assoc( $sql ) ){
-			echo "got into while loop";
-
 			if( $email === $rows['email'] && $password === $rows['password'] ){ //login matches data
-
-				echo "got into if statement";
+				
 				$loginUnsuccessful = false;
 				session_start();
 
 				$_SESSION["name"] = $rows['name']; //store their name
 				$_SESSION["username"] = $rows['username']; //store their username
 
-				echo $_SESSION["name"];
+
 
 				if( $rows['user'] === "guest" ){ //guest logs in
 					$_SESSION["status"] = "guest"; //status of user indicated
 					// header('Location: /webroot/finalWebMini/redirect.php');
-					echo "guest";
 				}
 				else if( $rows['user'] === "admin" ){
 					$_SESSION["status"] = "admin";
-					echo "admin";
 					// header('Location: /webroot/finalWebMini/addPost.html');
 				}
 				echo "status: ". $_SESSION["status"];
-				echo "name:" . $_SESSION["name"];
+				echo " name:" . $_SESSION["name"];
 				//header('Location: /index.php');
 			}
 			else {
