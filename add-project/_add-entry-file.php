e903c4fbec0a158c7f5b7e4cba477c20583a68e9
Durra Shuazlan <d.shuazlan@se19.qmul.ac.uk>
2022-10-15 23:01:24 +0100

    Fix

--- add-project/_add-entry-file.php
+++ add-project/_add-entry-file.php
@@ -6,7 +6,6 @@ $posts = mysqli_query( $conn, "SELECT * FROM projects ORDER BY ID DESC LIMIT 1" 
 or die( "Query unsuccessful sdfsdf" . mysqli_error( $conn ) );
 
 if( $posts -> num_rows > 0 ){
-    echo "more than one row : : ";
     while( $rows = mysqli_fetch_assoc( $posts ) ){
         echo $rows['title'] . $rows['id'] . "testing";
         $project_title = $rows['title'];
@@ -18,7 +17,6 @@ if( $posts -> num_rows > 0 ){
         fwrite( $myfile, $text );
         fclose( $myfile );
     }
-
 }
 
 header('Location: /"$project_title" . ".php"');
