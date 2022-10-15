52f01bf1512f5d206f58aa05ce837273cdc679f7
Durra Shuazlan <d.shuazlan@se19.qmul.ac.uk>
2022-10-15 14:57:09 +0100

    Add test

--- components/_head-header.php
+++ components/_head-header.php
@@ -11,7 +11,6 @@
 <?php
 include '../includes/connect.php'; //if file inside another folder
 include 'includes/connect.php'; //if file in no specific folder
-session_start();
 
 $uri_path = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
 $uri_segments = explode( '/', $uri_path );
--- test.php
+++ test.php
@@ -1,6 +1,14 @@
+<!DOCTYPE html>
+<html>
+<?php require 'components/_head-header.php' ?>
+
+
 <?php
  if(!isset($_SESSION)) {
     session_start();
 }
  echo "status: ". $_SESSION['status'];
 ?>
+<?php require 'components/_footer.php' ?>
+</body>
+</html>
