844d0bc85effc5bb34ac1d1b0af390c38e55b944
Durra Shuazlan <d.shuazlan@se19.qmul.ac.uk>
2022-10-15 15:34:27 +0100

    Add php test

--- test.php
+++ test.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html>
-<?php require 'components/_head-header.php' ?>
-
-What thE FUCK
-
 <?php
- if(!isset($_SESSION)) {
-    session_start();
-}
- echo "status: ". $_SESSION['status'];
+   phpinfo();
 ?>
-
-<?php require 'components/_footer.php' ?>
-</body>
-</html>
